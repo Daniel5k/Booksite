da079797c9ad88eb35ec4ec36d4691f9949d4535
Daniel <daniel@gmail.com>
2025-07-11 02:24:17 +0100

    first commit

--- booksite/settings.py
+++ booksite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@px)0lk=12qvyj#zva^f9hc-$7brys2y90-8m57nw1z77hib#!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
